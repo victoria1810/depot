c7c0ec629f9e1ec31e84b7f46805384c58b37ccd
victoria <torri91@list.ru>
2018-09-19 14:20:55 +0700

    added consumer cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :carts
+  resources :line_items
+  get 'store/index'
+
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root to: 'store#index', as: 'store'
   # root 'welcome#index'
 
   # Example of regular route:
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                          description: "fvygbuhjkml",
+                          image_url: "zzz.jpg" )
+    product.price = -1
+      assert product.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+      assert product.invalid?
+      assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+      assert product.valid?
+  end
+
 end
